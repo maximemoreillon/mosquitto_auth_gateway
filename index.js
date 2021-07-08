f524e8fb90edd2a41f9f00d5dd69ee972a3ae331
Maxime Moreillon <m.moreillon@gmail.com>
2021-07-08 22:01:38 +0900

    now login using username and password

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ function get_user_using_jwt(jwt){
   return axios.get(url, options)
 }
 
+function login(credentials){
+  const url = `${USER_MANAGER_API_URL}/login`
+  return axios.post(url, credentials)
+}
+
 function user_is_superuser(user){
   return user.properties?.isAdmin
     ?? user.admin
@@ -45,10 +50,12 @@ function get_user_id(user){
 
 app.post('/getuser', (req, res) => {
 
-  get_user_using_jwt(req.body.username)
+  //get_user_using_jwt(req.body.username)
+  login(req.body)
   .then(({data}) => {
-    const user_id = get_user_id(data)
-    console.log(`Successful connection from user ${user_id}`)
+    //const user_id = get_user_id(data)
+    //console.log(`Successful connection from user ${user_id}`)
+    console.log(`Successful connection from user ${req.body.username}`)
     res.send('OK')
    })
   .catch(error => {
@@ -59,47 +66,61 @@ app.post('/getuser', (req, res) => {
 })
 
 app.post('/superuser', (req, res) => {
-  get_user_using_jwt(req.body.username)
-  .then(({data}) => {
 
-    const user_id = get_user_id(data)
-
-    if(user_is_superuser(data)) {
-      console.log(`User ${user_id} is superuser`)
-      res.send('OK')
-    }
-    else {
-      console.log(`User ${user_id} is NOT superuser`)
-      res.status(403).send('Not OK')
-    }
-
-  })
-  .catch(error => {
-    console.log(error)
-    res.status(403).send(error)
-  })
+  console.log('Superuser deactivated')
+  res.status(403).send('Not OK')
+  // get_user_using_jwt(req.body.username)
+  // .then(({data}) => {
+  //
+  //   const user_id = get_user_id(data)
+  //
+  //   if(user_is_superuser(data)) {
+  //     console.log(`User ${user_id} is superuser`)
+  //     res.send('OK')
+  //   }
+  //   else {
+  //     console.log(`User ${user_id} is NOT superuser`)
+  //     res.status(403).send('Not OK')
+  //   }
+  //
+  // })
+  // .catch(error => {
+  //   console.log(error)
+  //   res.status(403).send(error)
+  // })
 })
 
 app.post('/aclcheck', (req, res) => {
   // req.body.acc: 1 subscribe, 2 publish
-  get_user_using_jwt(req.body.username)
-  .then(({data}) => {
-    const user_id = get_user_id(data)
-    const {topic} = req.body
 
-    if(topic.startsWith(`/${user_id}/`)) {
-      console.log(`User ${user_id} is allowed to use topic ${topic}`)
-      res.send('OK')
-    }
-    else {
-      console.log(`User ${user_id} is NOT allowed to use topic ${topic}`)
-      res.status(403).send('Not OK')
-    }
-  })
-  .catch(error => {
-    console.log(error)
-    res.status(403).send(error)
-  })
+  const {username} = req.body
+  if(topic.startsWith(`/${username}/`)) {
+    console.log(`User ${username} is allowed to use topic ${topic}`)
+    res.send('OK')
+  }
+  else {
+    console.log(`User ${username} is NOT allowed to use topic ${topic}`)
+    res.status(403).send('Not OK')
+  }
+
+  // get_user_using_jwt(req.body.username)
+  // .then(({data}) => {
+  //   const user_id = get_user_id(data)
+  //   const {topic} = req.body
+  //
+  //   if(topic.startsWith(`/${user_id}/`)) {
+  //     console.log(`User ${user_id} is allowed to use topic ${topic}`)
+  //     res.send('OK')
+  //   }
+  //   else {
+  //     console.log(`User ${user_id} is NOT allowed to use topic ${topic}`)
+  //     res.status(403).send('Not OK')
+  //   }
+  // })
+  // .catch(error => {
+  //   console.log(error)
+  //   res.status(403).send(error)
+  // })
 })
 
 
