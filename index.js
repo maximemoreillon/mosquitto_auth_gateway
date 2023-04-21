8ef0bf4f871f5db2743f923226fc9a634435b977
Maxime Moreillon <m.moreillon@gmail.com>
2023-04-22 08:53:11 +0900

    fixing mqtt topic synbtax

--- index.js
+++ index.js
@@ -1,91 +1,76 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const axios = require('axios')
-// const apiMetrics = require('prometheus-api-metrics')
-const cors = require('cors')
-const {version, author} = require('./package.json')
-const createHttpError = require('http-errors')
-const jwt = require('jsonwebtoken')
+const express = require("express")
+require("express-async-errors")
+const dotenv = require("dotenv")
+const axios = require("axios")
+const apiMetrics = require("prometheus-api-metrics")
+const cors = require("cors")
+const { version, author } = require("./package.json")
+const createHttpError = require("http-errors")
+const jwt = require("jsonwebtoken")
 
 dotenv.config()
 
 const {
   APP_PORT = 80,
-  IDENTIFICATION_URL = 'http://user-manager/users/self',
-  LOGIN_URL = 'http://user-manager/auth/login',
+  IDENTIFICATION_URL = "http://user-manager/users/self",
+  LOGIN_URL = "http://user-manager/auth/login",
 } = process.env
-
-
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-// app.use(apiMetrics())
+app.use(apiMetrics())
 
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send({
-    application_name: 'Mosquitto Auth Gateway',
+    application_name: "Mosquitto Auth Gateway",
     author,
     version,
     auth: {
       identification_url: IDENTIFICATION_URL,
       login_url: LOGIN_URL,
-    }
+    },
   })
 })
 
 const get_user_using_token = (token) => {
   const headers = { Authorization: `Bearer ${token}` }
-  return axios.get(IDENTIFICATION_URL, {headers})
+  return axios.get(IDENTIFICATION_URL, { headers })
 }
 
-const login = credentials => axios.post(LOGIN_URL, credentials)
+const login = (credentials) => axios.post(LOGIN_URL, credentials)
 
-const user_is_superuser = (user) => user.admin
-    ?? user.isAdmin
-    ?? user.administrator
-    ?? user.isAdministrator
+const user_is_superuser = (user) =>
+  user.admin ?? user.isAdmin ?? user.administrator ?? user.isAdministrator
 
-app.post('/getuser', async (req, res, next) => {
-
+app.post("/getuser", async (req, res, next) => {
   // username can be a JWT
-  const {username, password} = req.body
+  const { username, password } = req.body
 
   try {
     if (jwt.decode(username)) await get_user_using_token(username)
     else await login({ username, password })
-    res.send('OK')
-  } 
-  catch (error) {
+    res.send("OK")
+  } catch (error) {
     next(error)
   }
-
 })
 
-
-app.post('/superuser', async (req, res, next) => {
-
+app.post("/superuser", async (req, res) => {
   // Note: Body only contains username and thus NOT password
   // Thus, superuser check only works when using JWT and not credentials
-  const { username: token} = req.body
+  const { username: token } = req.body
 
-  try {
-    if (!jwt.decode(token)) throw 'Username is not token'
-    const { data: user} = await get_user_using_token(username)
-    if (!user_is_superuser(user)) throw createHttpError(403, `User is not administrator`)
-    res.send('OK')
-  } 
-  catch (error) {
-    next(error)
-  }
-
+  if (!jwt.decode(token)) throw "Username is not token"
+  const { data: user } = await get_user_using_token(username)
+  if (!user_is_superuser(user))
+    throw createHttpError(403, `User is not administrator`)
+  res.send("OK")
 })
 
-app.post('/aclcheck', async (req, res, next) => {
-
-  const { username, topic, acc} = req.body
+app.post("/aclcheck", async (req, res) => {
+  const { username, topic, acc } = req.body
   // Note: Body does NOT contain password
   // However, username can be JWT
   // acc: 1 subscribe, 2 publish ??
@@ -94,23 +79,24 @@ app.post('/aclcheck', async (req, res, next) => {
   let actualUsername
 
   try {
-    if (!jwt.decode(username)) throw 'Username is not token'
+    if (!jwt.decode(username)) throw "Username is not token"
     const { data } = await get_user_using_token(username)
     actualUsername = data.username
   } catch (error) {
     actualUsername = username
   }
 
-  try {
-    // Only allow users to manipulate topics that contain their username
-    if (!topic.startsWith(`/${actualUsername}/`)) throw createHttpError(403, `User ${actualUsername} is not allowed to use topic ${topic}`)
-    res.send('OK')
-  }
-  catch (error) {
-    next(error)
-  }
-
-
+  // Only allow users to manipulate topics that contain their username
+  // NOTE: topic were privosuly starting with '/', which is bad practice
+  if (
+    !topic.startsWith(`${actualUsername}/`) ||
+    !topic.startsWith(`/${actualUsername}/`)
+  )
+    throw createHttpError(
+      403,
+      `User ${actualUsername} is not allowed to use topic ${topic}`
+    )
+  res.send("OK")
 })
 
 // Express error handler
@@ -121,8 +107,8 @@ app.use((error, req, res, next) => {
   res.status(statusCode).send(message)
 })
 
-
-
 app.listen(APP_PORT, () => {
-  console.log(`Mosquitto Auth Gateway v${version} listening on port ${APP_PORT}`)
+  console.log(
+    `Mosquitto Auth Gateway v${version} listening on port ${APP_PORT}`
+  )
 })
